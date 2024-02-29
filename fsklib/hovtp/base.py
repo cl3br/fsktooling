3adb984e12dd67eb018e094c5a79ab2aa5393769
Clemens Brummer <clemens.brummer@gmail.com>
2024-02-29 03:43:11 +0100

    add hovtp multiplexer

--- fsklib/hovtp/base.py
+++ fsklib/hovtp/base.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from http.client import HTTPConnection
+from http.client import HTTPConnection, NotConnected
+import random
 import traceback
+from typing import Optional
 
 
 class OdfListener(BaseHTTPRequestHandler):
@@ -8,7 +10,7 @@ class OdfListener(BaseHTTPRequestHandler):
         self.send_response_only(200)
         self.send_header("Cache-Control", "no-cache")
         self.send_header("X-HOVTP-Environment", self.headers["X-HOVTP-Environment"])
-        self.send_header("X-HOVTP-Last-Serial-Number", self.headers["X-HOVTP-Last-Serial-Number"])
+        self.send_header("X-HOVTP-Last-Serial-Number", self.headers["X-HOVTP-Serial-Number"])
         self.end_headers()
 
     def do_OPTIONS(self):
@@ -44,17 +46,21 @@ class OdfSender:
     def __init__(self, host_name: str, port="8080"):
         self.connection = HTTPConnection(host_name, port, timeout=1)
 
-    def send_odf(self, odf: str, odf_type: str):
+    def send_odf(self, odf: str, serial_number: Optional[int] = None):        
         odf_bytes = odf.encode("utf-8")
-        headers = {"Cache-Control": "no-store, no-cache",
-                   "X-HOVTP-Session-Id": "a4e244de-97ad-48e9-9957-e4b5ad94b624",
+        if not serial_number:
+            serial_number = random.randrange(999999999)
+        headers = {"X-HOVTP-Session-Id": "a4e244de-97ad-48e9-9957-e4b5ad94b624",
                    "X-HOVTP-Environment": "Production",
                    "X-HOVTP-Venue": "FSK",
                    "X-HOVTP-Discipline": "FSK",
                    "X-HOVTP-Origin": "OVR-FSK",
                    "X-HOVTP-Data-Type": "ODF",
-                   "X-ODF-Type": odf_type,
+                   "X-HOVTP-Serial-Number": str(serial_number),
+                   "Content-Type": "text/xml",
                    "Host": self.connection.host,
+                   "Cache-Control": "no-store, no-cache",
+                   "Pragma": "no-cache",
                    "Content-Length": len(odf_bytes),
                    "Connection": "Keep-Alive"}
         try:
@@ -63,12 +69,24 @@ class OdfSender:
             print(r.code)
             if r.code != 200:
                 print("Error")
+            if r.headers["X-HOVTP-Last-Serial-Number"] != str(serial_number):
+                print("Warning. Serial number does not match.")
             print(r.read().decode())
         except:
-            print("No connection")
-            traceback.print_exc()
+            print(f"No connection for {self.connection.host}:{self.connection.port}")
+            # traceback.print_exc()
             pass
 
 
+class OdfMultiplexer(OdfListener):
+    def __init__(self, request, client_address, server):
+        self.senders = [OdfSender("localhost", "11112"), OdfSender("localhost", "11113")]
+        super().__init__(request, client_address, server)
+
+    def process_odf(self, odf: str):
+        for sender in self.senders:
+            sender.send_odf(odf)
+
+
 if __name__ == "__main__":
-    run_server(OdfListener, "localhost", 11111)
+    run_server(OdfMultiplexer, "localhost", 11111)
