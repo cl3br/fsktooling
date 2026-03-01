c3566e78a37e165368517ae6f2edb957180e1aa5
Clemens Brummer <clemens.brummer@gmail.com>
2026-03-01 04:49:30 +0100

    fix: make club entry optional for get entries / starting order

--- scripts/get_entries_from_fsm_database.py
+++ scripts/get_entries_from_fsm_database.py
@@ -20,7 +20,7 @@ else:
 cursor.execute("SELECT Id, Name, Level, Type, SortOrder FROM category")
 for (cat_id, cat_name, cat_level, cat_type, cat_order) in list(cursor):
     print(cat_name)
-    cat_type = model.CategoryType.from_value(cat_type, model.DataSource.FSM)
+    cat_type: model.CategoryType | None = model.CategoryType.from_value(cat_type, model.DataSource.FSM)
     if cat_type is None:
         cat_type = model.CategoryType.SINGLES
     cat_level = model.CategoryLevel.from_value(cat_level, model.DataSource.FSM)
@@ -34,17 +34,22 @@ for (cat_id, cat_name, cat_level, cat_type, cat_order) in list(cursor):
         segments.append(model.Segment(name, short_name, model.SegmentType.from_value(segment_type, model.DataSource.FSM)))
 
     if cat_type in [model.CategoryType.MEN, model.CategoryType.WOMEN]: # singles
-        cursor.execute("SELECT person.FederationId, person.FirstName, person.LastName, person.BirthDate, person.Gender, person.Nation_Id, club.Name, person.Club "
+        cursor.execute("SELECT person.FederationId, person.FirstName, person.LastName, person.BirthDate, person.Gender, person.Nation_Id, person.Club "
                        "FROM entry "
                        "    JOIN single ON entry.Competitor_Id = single.Competitor_Id_Pk "
                        "    JOIN person ON person.Id = single.Person_Id "
-                       "    JOIN club ON club.ShortName = person.Club "
                       f"WHERE entry.Category_Id = {str(cat_id)} "
                        "ORDER BY entry.Id ASC")
 
-        for (number, (id, first_name, last_name, bday, gender, nation, club_name, club_abbr)) in enumerate(cursor, 1):
+        for (number, (id, first_name, last_name, bday, gender, nation, club_abbr)) in enumerate(list(cursor), 1):
             print("%s %s" % (first_name, last_name))
-            person = model.Person(id, first_name, last_name, model.Gender.from_value(gender, model.DataSource.FSM), bday, club)
+            if club_abbr is None:
+                club_abbr = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr + "'")
+            result = cursor.fetchone()
+            club_name = result[0] if result is not None else ""
+            cursor.reset()
+            person = model.Person(id, first_name, last_name, model.Gender.from_value(gender, model.DataSource.FSM), bday, model.Club(club_name, club_abbr, nation))
             participant = model.ParticipantSingle(cat, person)
             if fake_start_number:
                 for segment in segments:
@@ -53,18 +58,27 @@ for (cat_id, cat_name, cat_level, cat_type, cat_order) in list(cursor):
                 csv.add_participant(participant)
 
     elif cat_type in [model.CategoryType.PAIRS, model.CategoryType.ICEDANCE]:  # pairs
-        cursor.execute("SELECT partner1.FederationId, partner1.FirstName, partner1.LastName, partner1.BirthDate, partner1.Gender, partner1.Nation_Id, club1.Name, partner1.Club,"
-                       "       partner2.FederationId, partner2.FirstName, partner2.LastName, partner2.BirthDate, partner2.Gender, partner2.Nation_Id, club2.Name, partner2.Club "
+        cursor.execute("SELECT partner1.FederationId, partner1.FirstName, partner1.LastName, partner1.BirthDate, partner1.Gender, partner1.Nation_Id, partner1.Club,"
+                       "       partner2.FederationId, partner2.FirstName, partner2.LastName, partner2.BirthDate, partner2.Gender, partner2.Nation_Id, partner2.Club "
                        "FROM entry "
                        "    JOIN couple ON entry.Competitor_Id = couple.Competitor_Id_Pk "
                        "    JOIN person AS partner1 ON partner1.Id = couple.PersonLady_Id "
                        "    JOIN person AS partner2 ON partner2.Id = couple.PersonMale_Id "
-                       "    JOIN club AS club1 ON club1.ShortName = partner1.Club "
-                       "    JOIN club AS club2 ON club2.ShortName = partner2.Club "
                        "WHERE entry.Category_Id = " + str(cat_id))
 
-        for (number, (id1, first_name1, last_name1, bday1, gender1, nation1, club_name1, club_abbr1, id2, first_name2, last_name2, bday2, gender2, nation2, club_name2, club_abbr2)) in enumerate(cursor, 1):
+        for (number, (id1, first_name1, last_name1, bday1, gender1, nation1, club_abbr1, id2, first_name2, last_name2, bday2, gender2, nation2, club_abbr2)) in enumerate(list(cursor), 1):
             print("%s %s / %s %s" % (first_name1, last_name1, first_name2, last_name2))
+            if club_abbr1 is None:
+                club_abbr1 = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr1 + "'")
+            result = cursor.fetchone()
+            club_name1 = result[0] if result is not None else ""
+            cursor.reset()
+            if club_abbr2 is None:
+                club_abbr2 = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr2 + "'")
+            result = cursor.fetchone()
+            club_name2 = result[0] if result is not None else ""
             club1 = model.Club(club_name1, club_abbr1, nation1)
             club2 = model.Club(club_name2, club_abbr2, nation2)
             partner1 = model.Person(id1, first_name1, last_name1, model.Gender.from_value(gender1, model.DataSource.FSM), bday1, club1)
@@ -78,14 +92,19 @@ for (cat_id, cat_name, cat_level, cat_type, cat_order) in list(cursor):
                 csv.add_participant(participant)
     elif cat_type == model.CategoryType.SYNCHRON:
         cursor.execute(
-            "SELECT synchronizedteam.FederationId, synchronizedteam.Name, synchronizedteam.Nation_Id, club.Name, synchronizedteam.Club "
+            "SELECT synchronizedteam.FederationId, synchronizedteam.Name, synchronizedteam.Nation_Id, synchronizedteam.Club "
             "FROM entry "
             "    JOIN synchronizedteam ON entry.Competitor_Id = synchronizedteam.Competitor_Id_Pk "
-            "    JOIN club ON club.ShortName = synchronizedteam.Club "
             "WHERE entry.Category_Id = " + str(cat_id))
 
-        for (number, (id, name, nation, club_name, club_abbr)) in enumerate(cursor, 1):
+        for (number, (id, name, nation, club_abbr)) in enumerate(list(cursor), 1):
             print("%s" % (name))
+            if club_abbr is None:
+                club_abbr = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr + "'")
+            result = cursor.fetchone()
+            club_name = result[0] if result is not None else ""
+            cursor.reset()
             club = model.Club(club_name, club_abbr, nation)
             sys_team = model.Team(id, name, club, [])
             participant = model.ParticipantTeam(cat, sys_team)
--- scripts/get_starting_order_from_fsm_database.py
+++ scripts/get_starting_order_from_fsm_database.py
@@ -9,6 +9,8 @@ from fsklib.output import ParticipantCsvOutput
 con = mysql.connector.connect(user='sa', password='fsmanager', host='127.0.0.1', database='test')
 
 cursor = con.cursor()
+if cursor is None:
+    raise Exception("Could not create database cursor")
 
 csv = ParticipantCsvOutput(Path('starting_order.csv'))
 
@@ -21,7 +23,7 @@ cursor.execute("SELECT segment.Id, segment.Name, segment.ShortName, segment.Segm
 
 for (seg_id, seg_name, seg_short_name, seg_type, cat_name, cat_level, cat_type, cat_order) in list(cursor):
     print(f"{cat_name} - {seg_name}")
-    cat_type = model.CategoryType.from_value(cat_type, model.DataSource.FSM)
+    cat_type: model.CategoryType | None = model.CategoryType.from_value(cat_type, model.DataSource.FSM)
     if cat_type is None:
         cat_type = model.CategoryType.WOMEN
     cat_level = model.CategoryLevel.from_value(cat_level, model.DataSource.FSM)
@@ -35,17 +37,22 @@ for (seg_id, seg_name, seg_short_name, seg_type, cat_name, cat_level, cat_type, 
 
     # singles
     if cat_type in [model.CategoryType.MEN, model.CategoryType.WOMEN]:
-        cursor.execute("SELECT person.FederationId, person.FirstName, person.LastName, person.BirthDate, person.Gender, person.Nation_Id, club.Name, person.Club, competitorresult.StartNumber "
+        cursor.execute("SELECT person.FederationId, person.FirstName, person.LastName, person.BirthDate, person.Gender, person.Nation_Id, person.Club, competitorresult.StartNumber "
                         "FROM competitorresult "
                         "    JOIN entry ON competitorresult.Entry_Id = entry.Id "
                         "    JOIN single ON entry.Competitor_Id = single.Competitor_Id_Pk "
                         "    JOIN person ON person.Id = single.Person_Id "
-                        "    JOIN club ON club.ShortName = person.Club "
                         f"WHERE competitorresult.Segment_Id = {str(seg_id)} "
                         "ORDER BY competitorresult.StartNumber ASC")
 
-        for (id, first_name, last_name, bday, gender, nation, club_name, club_abbr, start_number) in cursor:
+        for (id, first_name, last_name, bday, gender, nation, club_abbr, start_number) in list(cursor):
             print("%d - %s %s" % (start_number, first_name, last_name))
+            if club_abbr is None:
+                club_abbr = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr + "'")
+            result = cursor.fetchone()
+            club_name = result[0] if result is not None else ""
+            cursor.reset()
             club = model.Club(club_name, club_abbr, nation)
             person = model.Person(id, first_name, last_name, model.Gender.from_value(gender, model.DataSource.FSM), bday, club)
             participant = model.ParticipantSingle(cat, person, model.Role.ATHLETE)
@@ -53,20 +60,29 @@ for (seg_id, seg_name, seg_short_name, seg_type, cat_name, cat_level, cat_type, 
 
     # pairs / ice dance
     elif cat_type in [model.CategoryType.PAIRS, model.CategoryType.ICEDANCE]:
-        cursor.execute("SELECT partner1.FederationId, partner1.FirstName, partner1.LastName, partner1.BirthDate, partner1.Gender, partner1.Nation_Id, club1.Name, partner1.Club,"
-                        "       partner2.FederationId, partner2.FirstName, partner2.LastName, partner2.BirthDate, partner2.Gender, partner2.Nation_Id, club2.Name, partner2.Club,"
+        cursor.execute("SELECT partner1.FederationId, partner1.FirstName, partner1.LastName, partner1.BirthDate, partner1.Gender, partner1.Nation_Id, partner1.Club,"
+                        "       partner2.FederationId, partner2.FirstName, partner2.LastName, partner2.BirthDate, partner2.Gender, partner2.Nation_Id, partner2.Club,"
                         "       competitorresult.StartNumber "
                         "FROM competitorresult "
                         "    JOIN entry ON competitorresult.Entry_Id = entry.Id "
                         "    JOIN couple ON entry.Competitor_Id = couple.Competitor_Id_Pk "
                         "    JOIN person AS partner1 ON partner1.Id = couple.PersonLady_Id "
                         "    JOIN person AS partner2 ON partner2.Id = couple.PersonMale_Id "
-                        "    JOIN club AS club1 ON club1.ShortName = partner1.Club "
-                        "    JOIN club AS club2 ON club2.ShortName = partner2.Club "
                         "WHERE competitorresult.Segment_Id = " + str(seg_id))
 
-        for (id1, first_name1, last_name1, bday1, gender1, nation1, club_name1, club_abbr1, id2, first_name2, last_name2, bday2, gender2, nation2, club_name2, club_abbr2, start_number) in cursor:
+        for (id1, first_name1, last_name1, bday1, gender1, nation1, club_abbr1, id2, first_name2, last_name2, bday2, gender2, nation2, club_abbr2, start_number) in list(cursor):
             print("%d - %s %s / %s %s" % (start_number, first_name1, last_name1, first_name2, last_name2))
+            if club_abbr1 is None:
+                club_abbr1 = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr1 + "'")
+            result = cursor.fetchone()
+            club_name1 = result[0] if result is not None else ""
+            cursor.reset()
+            if club_abbr2 is None:
+                club_abbr2 = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr2 + "'")
+            result = cursor.fetchone()
+            club_name2 = result[0] if result is not None else ""
             club1 = model.Club(club_name1, club_abbr1, nation1)
             club2 = model.Club(club_name2, club_abbr2, nation2)
             partner1 = model.Person(id1, first_name1, last_name1, model.Gender.from_value(gender1, model.DataSource.FSM), bday1, club1)
@@ -78,15 +94,20 @@ for (seg_id, seg_name, seg_short_name, seg_type, cat_name, cat_level, cat_type, 
     # synchron
     elif cat_type == model.CategoryType.SYNCHRON:
         cursor.execute(
-            "SELECT synchronizedteam.FederationId, synchronizedteam.Name, synchronizedteam.Nation_Id, club.Name, synchronizedteam.Club, competitorresult.StartNumber "
+            "SELECT synchronizedteam.FederationId, synchronizedteam.Name, synchronizedteam.Nation_Id, synchronizedteam.Club, competitorresult.StartNumber "
             "FROM competitorresult "
             "    JOIN entry ON competitorresult.Entry_Id = entry.Id "
             "    JOIN synchronizedteam ON entry.Competitor_Id = synchronizedteam.Competitor_Id_Pk "
-            "    JOIN club ON club.ShortName = synchronizedteam.Club "
             "WHERE competitorresult.Segment_Id = " + str(seg_id))
 
-        for (id, name, nation, club_name, club_abbr, start_number) in cursor:
+        for (id, name, nation, club_abbr, start_number) in list(cursor):
             print("%d - %s" % (start_number, name))
+            if club_abbr is None:
+                club_abbr = ""
+            cursor.execute("SELECT Name FROM club WHERE ShortName = '" + club_abbr + "'")
+            result = cursor.fetchone()
+            club_name = result[0] if result is not None else ""
+            cursor.reset()
             club = model.Club(club_name, club_abbr, nation)
             sys_team = model.Team(id, name, club, [])
             participant = model.ParticipantTeam(cat, sys_team, model.Role.ATHLETE)
